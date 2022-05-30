57f9e19c018dc9a10f2f58b29176730303d2be70
YannickDenBoer <yannick.denboer@vangoghstraat.com>
2022-05-30 16:28:11 +0200

    Debugger complete

--- INFOGR2022Template/MainScene.cs
+++ INFOGR2022Template/MainScene.cs
@@ -22,18 +22,18 @@ namespace Template
 		Light light1;
 		public List<Light> lights;
 
-		public const int rayLength = int.MaxValue;
+		public const int rayLength = 5000;
 		// initialize
 		public void Init()
 		{
 			primitives = new List<Primitive>();
-			sphere1 = new Sphere(new Vector3(0, .1f, 1f), .1f, new Material(0, 0, 255));
-			sphere2 = new Sphere(new Vector3(-1, 1, 1.5f), .1f, new Material(255, 0, 0));
-			sphere3 = new Sphere(new Vector3(-1, 1, 2f), .4f, new Material(0, 0, 255));
+			sphere1 = new Sphere(new Vector3(0, 0, 1f), .1f, new Material(0, 0, 255));
+			sphere2 = new Sphere(new Vector3(-1, 0, 1.5f), .1f, new Material(255, 0, 0));
+			sphere3 = new Sphere(new Vector3(2, 0, 2f), .4f, new Material(0, 0, 255));
 			plane1 = new Plane(new Vector3(0, 1, 0), 2, new Material(255, 0, 255, false, false));
 			primitives.Add(sphere1);
-			//primitives.Add(sphere2);
-			//primitives.Add(sphere3);
+			primitives.Add(sphere2);
+			primitives.Add(sphere3);
 			primitives.Add(plane1);
 			camera = new Camera();
 			ray = new Ray(Vector3.Zero, Vector3.One, rayLength);
@@ -217,22 +217,23 @@ namespace Template
 		//normaallijn moet nog berekend worden bij een intersect
 		//Intersect werkt niet als de ray in de sphere begint
 		//Plane intersect moet nog worden gemaakt
+		//Distance werkt niet vgm
 	}
-	class Raytracer 
+	class Raytracer
 	{
 		MainScene scene;
 		Camera cam;
 		Surface surface;
 
 		public Raytracer(MainScene scene, Camera cam, Surface surface)
-        {
+		{
 			this.scene = scene;
 			this.cam = cam;
 			this.surface = surface;
-        }
+		}
 
 		public Intersection CheckCollisions(Ray ray)
-        {
+		{
 			Intersection intersection = new Intersection();
 			foreach (Primitive primitive in scene.primitives)
 			{
@@ -249,45 +250,100 @@ namespace Template
 		}
 
 		public void Render()
-        {
+		{
 			for (int y = 0; y < scene.screen.height; y++)
 			{
-				for (int x = 0; x < scene.screen.width; x++)
+				for (int x = surface.width - surface.width / 2; x < scene.screen.width; x++) //2e helft van het scherm tot eind
 				{
 					scene.ray.direction = Vector3.Normalize(
-						new Vector3(cam.scrnTL.X - (cam.scrnTL.X - cam.scrnBR.X) * ((float)x / scene.screen.width),
-						cam.scrnTL.Y - (cam.scrnTL.Y - cam.scrnBR.Y) * ((float)y / scene.screen.height), 
+						new Vector3(cam.scrnTL.X - (cam.scrnTL.X - cam.scrnBR.X) * (((float)x - surface.width / 2) / (surface.width / 2)),
+						cam.scrnTL.Y - (cam.scrnTL.Y - cam.scrnBR.Y) * ((float)y / scene.screen.height),
 						cam.scrnTL.Z)
-						);
-					Intersection intersection1 = CheckCollisions(scene.ray);
-					
+						); //find ray direction
+					Intersection intersection1 = CheckCollisions(scene.ray); //check if ray intersects with an object in the scene
+
 					if (intersection1.nearestPrim != null)
-                    {
+					{
 						Vector3 intersectionPoint = scene.ray.origin + scene.ray.direction * scene.ray.length;
 						foreach (Light light in scene.lights)
-                        {
+						{
 							Vector3 direction = Vector3.Normalize(new Vector3(light.position - intersectionPoint));
 							float length = new Vector3(light.position - intersectionPoint).Length;
 							Ray reflectionRay = new Ray(intersectionPoint, direction, length);
 							Intersection intersection2 = CheckCollisions(reflectionRay);
-							
+
 							float reflectedEnergy = Math.Min(1, Math.Max(0.1f, 1 / (float)Math.Pow(length, 2) * 1f * Vector3.Dot(Vector3.Normalize(intersection1.normal), direction)));
 
-                            if (!intersection1.nearestPrim.mat.isCheckered)
-                            {
-								surface.pixels[x + y * scene.screen.width] = intersection1.nearestPrim.GetColor(reflectedEnergy, intersection1);
+							if (!intersection1.nearestPrim.mat.isCheckered)
+							{
+								surface.pixels[(x + y * surface.width)] = intersection1.nearestPrim.GetColor(reflectedEnergy, intersection1);
 							}
-                            else
-                            {
-								surface.pixels[x + y * scene.screen.width] = intersection1.nearestPrim.GetColor(intersectionPoint.X, intersectionPoint.Y);
+							else
+							{
+								surface.pixels[(x + y * surface.width)] = intersection1.nearestPrim.GetColor(intersectionPoint.X, intersectionPoint.Y);
 							}
-							
-							scene.ray.length = MainScene.rayLength;
 						}
+					}
+					//DEBUG OUTPUT:
+					//Project rays
+					if (scene.ray.direction.Y == 0)
+					{
+						if (x % 30 == 0)
+						{
+							surface.Line(
+								TX(scene.ray.origin.X), 
+								TY(scene.ray.origin.Z), 
+								TX(scene.ray.origin.X + scene.ray.length * scene.ray.direction.X), 
+								TY(scene.ray.origin.Z + scene.ray.length * scene.ray.direction.Z),
+								MainScene.MixColor(255, 255,0));
+						}
+
+					}
+					scene.ray.length = MainScene.rayLength;
+				}
+			}
+			//MORE DEBUG OUTPUT
+			//Project camera position
+			surface.Box(TX(cam.position.X), TY(cam.position.Z), TX(cam.position.X), TY(cam.position.Z), 255);
+			//Project screen plane
+			surface.Line(TX(cam.scrnTL.X), TY(cam.scrnTL.Z), TX(cam.scrnBR.X), TY(cam.scrnBR.Z), MainScene.MixColor(255,255,255));
+
+			//Project spheres
+			float pistep = (float)Math.PI / 100;
+			foreach (Primitive prim in scene.primitives)
+			{
+				if (prim is Sphere)
+				{
+					for (float i = 0; i < 2 * Math.PI; i = i + pistep)
+					{
+						surface.Line(
+							TX((float)(((Sphere)prim).position.X + Math.Cos(i) * ((Sphere)prim).radius)), 
+							TY((float)(((Sphere)prim).position.Z + Math.Sin(i) * ((Sphere)prim).radius)), 
+							TX((float)(((Sphere)prim).position.X + Math.Cos(i + pistep) * ((Sphere)prim).radius)), 
+							TY((float)(((Sphere)prim).position.Z + Math.Sin(i + pistep) * ((Sphere)prim).radius)), 
+							255);
 					}
 				}
 			}
 		}
+
+		public int TX(float point)
+		{
+			float shift = point + 2; //make -2:2 into 0:4
+			float scale = (shift * (surface.width/2))/4; //make 0:4 into 0:512
+			return Convert.ToInt32(scale);
+		}
+
+		public int TY(float point)
+		{
+			float invert = -point;
+			float shift = invert + 3;
+			float scale = shift * (surface.width / 8);
+			
+			//4*160;4*100
+			return Convert.ToInt32(scale);
+		}
+
 	}
 
 	class Application 
